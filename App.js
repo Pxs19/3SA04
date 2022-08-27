5d9468e407f3939dcf10e25ec4f05a65f62be908
Panuvit Suckchuai <panuvit1165@gmail.com>
2022-08-27 16:02:48 +0700

    Part 5: Router

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Weather from './components/Weather';
-import Constants from 'expo-constants';
+// import { StyleSheet, Text, View } from 'react-native';
+// import Weather from './components/Weather';
+// import Constants from 'expo-constants';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ZipCodeScreen from './screen/ZipCodeScreen';
+import WeatherScreen from './screen/WeatherScreen';
+
+const Stack = createStackNavigator();
 
 
 export default function App() {
@@ -11,20 +17,28 @@ export default function App() {
     console.log("Hello from console")
   }
   return (
-    <View style={styles.container}>
-      <Weather zipCode = "90110"/>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Weather zipCode = "90110"/>
+    //   <StatusBar style="auto" />
+    // </View>
+
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     paddingTop: Constants.statusBarHeight,
+//   },
+// });
