6e98c610579ca05bc8655caa098494c4fbbfdbd2
Panuvit Suckchuai <panuvit1165@gmail.com>
2022-08-27 15:11:10 +0700

    Part 4: Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ImageBackground, Text, StyleSheet, View } from 'react-native';
 import Forecast from './Forecast';
 import Constants from 'expo-constants';
 
 
 export default function Weather(props) {
+
+
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=414dcf790bc29c3207a05f7c93f02d6d`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+
+        }
+
+    }, [props.zipCode])
 
     const [forecastInfo, setForecastInfo] = useState({
         main: 'main',
@@ -17,18 +39,12 @@ export default function Weather(props) {
 
     return (
 
-
-
         <ImageBackground source={require("../bg1.jpg")} style={styles.backdrop}>
 
-            <View style = {styles.highlight}>
+            <View style={styles.highlight}>
                 <Text style={styles.textStyle}>Zip Code is {props.zipCode}</Text>
                 <Forecast {...forecastInfo} />
             </View>
-
-
-
-
 
         </ImageBackground>
 
@@ -55,9 +71,9 @@ const styles = StyleSheet.create({
 
     textStyle: {
         fontSize: 20,
-         color: 'white',
-         textAlign: 'center'
-     },
+        color: 'white',
+        textAlign: 'center'
+    },
 
 
 });
